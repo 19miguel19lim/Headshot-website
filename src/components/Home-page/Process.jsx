810f0d74b97f14cec13ae239e0e0ad9ac8e7479a
Miguel Lim <19miguel19lim@gmail.com>
2023-10-18 15:49:14 +0800

    added animation

--- src/components/Home-page/Process.jsx
+++ src/components/Home-page/Process.jsx
@@ -6,6 +6,27 @@ function Process() {
   const female1 = "assets/female-img1.jpg";
   const female2 = "assets/female-img2.jpg";
 
+  const imgMovements = {
+    moveX: {
+      x: [
+        0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0,
+        -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0,
+        -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0,
+        -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0,
+      ],
+      transition: { duration: 400, yoyo: Infinity },
+    },
+    moveY: {
+      y: [
+        0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0,
+        -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0,
+        -100, 0, 0, -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0, 0,
+        -100, 0, -100, 0, -100, 0, 0, -100, 0, -100, 0, -100, 0,
+      ],
+      transition: { duration: 400, yoyo: Infinity },
+    },
+  };
+
   return (
     <div className="flex flex-col relative  mx-auto py-12 pb-44   space-y-12 overflowX-hidden ">
       <h1 className="text-4xl font-black text-center uppercase z-10 lg:text-7xl">
@@ -17,27 +38,56 @@ function Process() {
         <h1 className="px-44 md:px-[22rem] lg:px-[30rem]">.</h1>
 
         <motion.div
-          animate={{ scale: 1 }}
-          initial={{ scale: 0 }}
+          variants={imgMovements}
+          // initial="from"
+          animate="moveX"
           className="hidden md:block absolute right-44"
         >
           <img src={male} alt="" className=" w-[30rem] rounded-full" />
         </motion.div>
-        <div className="hidden lg:block absolute left-24 top-[50rem]">
-          <img src={male} alt="" className=" w-[25rem] rounded-full" />
-        </div>
-        <div className=" absolute right-30 top-4">
+        <motion.div
+          variants={imgMovements}
+          animate="moveX"
+          className="absolute left-24 top-[50rem]"
+        >
+          <img
+            src={male}
+            alt=""
+            className="lg:w-[22rem] xl:w-[25rem] rounded-full"
+          />
+        </motion.div>
+        <motion.div
+          variants={imgMovements}
+          animate="moveX"
+          className=" absolute right-30 top-4"
+        >
           <img src={female2} alt="" className=" w-60 rounded-full" />
-        </div>
-        <div className="hidden md:block absolute right-80 top-20">
+        </motion.div>
+        <motion.div
+          variants={imgMovements}
+          animate="moveY"
+          className="hidden md:block absolute right-80 top-20"
+        >
           <img src={female1} alt="" className=" w-20 rounded-full" />
-        </div>
-        <div className=" absolute right-[5rem] top-[40rem]">
-          <img src={female1} alt="" className=" w-[30rem] rounded-full" />
-        </div>
-        <div className="hidden md:block absolute right-[35rem] top-[18rem]">
+        </motion.div>
+        <motion.div
+          variants={imgMovements}
+          animate="moveY"
+          className=" absolute right-[5rem] top-[40rem]"
+        >
+          <img
+            src={female1}
+            alt=""
+            className="lg:w-[18rem] xl:w-[30rem] rounded-full"
+          />
+        </motion.div>
+        <motion.div
+          variants={imgMovements}
+          animate="moveY"
+          className="hidden md:block absolute right-[35rem] top-[18rem]"
+        >
           <img src={female2} alt="" className=" w-[15rem] rounded-full" />
-        </div>
+        </motion.div>
       </div>
 
       {/* 3-steps */}
